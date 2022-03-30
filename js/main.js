876c770ab7404250b119e84ede67946130b77796
Ilyas <ilyas.sagimbekov@gmail.com>
2022-03-30 08:25:19 +0600

    some animations

--- js/main.js
+++ js/main.js
@@ -1,9 +1,47 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
+        if(document.querySelector(".mob-nav").classList.contains('opened')){
+            document.querySelector(".mob-nav").classList.toggle('opened');
+            document.querySelector("ul.nav").classList.toggle('opened');
+        }
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
+});
+
+const openEls = document.querySelectorAll("[data-open]");
+const closeEls = document.querySelectorAll("[data-close]");
+const isVisible = "is-visible";
+
+for (const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+for (const el of closeEls) {
+  el.addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", e => {
+  // if we press the ESC
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.querySelector(".mob-nav").addEventListener("click", e => {
+    document.querySelector(".mob-nav").classList.toggle('opened');
+    document.querySelector("ul.nav").classList.toggle('opened');
 });
